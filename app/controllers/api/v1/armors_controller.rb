9a86bd8991c83901a81213e521989c421e5875d0
Ana Maria Benedetti <ambenedetti@gmail.com>
2020-07-06 14:25:13 +0200

    created controllers

---
+++ app/controllers/api/v1/armors_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::ArmorsController < ApplicationController
+end
---
+++ test/controllers/api/v1/armors_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::ArmorsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
