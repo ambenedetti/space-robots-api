e2ea58f65177e8e11b40e9fcd788bde4d66d1c91
Ana Maria Benedetti <ambenedetti@gmail.com>
2020-07-06 15:28:01 +0200

    updates weapons and armors controllers

--- app/controllers/api/v1/armors_controller.rb
+++ app/controllers/api/v1/armors_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::ArmorsController < ApplicationController
+  def index
+    @robot = Robot.find(params[:robot_id])
+    @armors = @robot.armors
+    render json: @armors
+  end
+
+  def show
+    @armor = Armor.find(params[:id])
+    render json: @armor
+  end
+
+  def create
+    @armor = Armor.new(armor_params)
+    if @armor.save
+      render json: @armor
+    else
+      render error: { error: "Armor not created" }, status: 400
+    end
+  end
+
+  def update
+    @robot = Robot.find(params[:robot_id])
+    @armor = @armor.weapons.find(params[:id])
+    if @armor
+      @armor.update(armor_params)
+      render json: { message: "Armor updated" }, status: 200
+    else
+      render error: { error: "Armor not updated" }, status: 400
+    end
+  end
+
+  def destroy
+    @armor = Armor.find(params[:id])
+    if @armor
+      @armor.destroy
+      render json: { message: "Armor deleted" }, status: 200
+    else
+      render error: { error: "Armor not updated" }, status: 400
+    end
+  end
+
+  private
+  def armor_params
+    params.require(:armor).permit(:armor_type, :robot_id, :serial_number)
+  end
 end
--- app/controllers/api/v1/weapons_controller.rb
+++ app/controllers/api/v1/weapons_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::WeaponsController < ApplicationController
+  def index
+    @robot = Robot.find(params[:robot_id])
+    @weapons = @robot.weapons
+    render json: @weapons
+  end
+
+  def show
+    @weapon = Weapon.find(params[:id])
+    render json: @weapon
+  end
+
+  def create
+    @weapon = Weapon.new(weapon_params)
+    if @weapon.save
+      render json: @weapon
+    else
+      render error: { error: "Weapon not created" }, status: 400
+    end
+  end
+
+  def update
+    @robot = Robot.find(params[:robot_id])
+    @weapon = @robot.weapons.find(params[:id])
+    if @weapon
+      @weapon.update(weapon_params)
+      render json: { message: "Weapon updated" }, status: 200
+    else
+      render error: { error: "Weapon not updated" }, status: 400
+    end
+  end
+
+  def destroy
+    @weapon = Weapon.find(params[:id])
+    if @weapon
+      @weapon.destroy
+      render json: { message: "Weapon deleted" }, status: 200
+    else
+      render error: { error: "Weapon not updated" }, status: 400
+    end
+  end
+
+  private
+  def weapon_params
+    params.require(:weapon).permit(:weapon_type, :robot_id, :serial_number)
+  end
 end
